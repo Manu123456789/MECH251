6f5e5539974e618de2a29f82bc90501d0ebec176
Manu123456789 <manu.bodagala@gmail.com>
2024-03-07 10:57:33 -0800

    Update function_class.m

--- function_class.m
+++ function_class.m
@@ -27,16 +27,45 @@ classdef GeometricObjectProcessor
             translated_node_xyz = node_xyz - centroid_rep;
         end
 
-        function sampled_points = sample_surface(obj, num_samples)
-            % Ensure obj.Vertices and obj.TriangulatedFaces are set
-            vertices = obj.Vertices;
-            triangulated_faces = obj.TriangulatedFaces;
-            
-            % Similar implementation as provided function...
-            % Calculation of areas, sampling points etc.
-            % At the end:
-            obj.SampledPoints = sampled_points; % Store the sampled points in the object property
-        end
+function sampled_points = sample_surface(vertices, triangulated_faces, num_samples)
+
+    % Calculating the area of each triangle
+    areas = zeros(1, size(triangulated_faces, 2)); % One area value per triangle
+    for i = 1:size(triangulated_faces, 2) % Iterate through each triangle
+        v1 = vertices(:, triangulated_faces(1, i)); % Vertex 1 of the i-th triangle
+        v2 = vertices(:, triangulated_faces(2, i)); % Vertex 2 of the i-th triangle
+        v3 = vertices(:, triangulated_faces(3, i)); % Vertex 3 of the i-th triangle
+        a = norm(v2 - v1);
+        b = norm(v3 - v2);
+        c = norm(v1 - v3);
+        s = 0.5 * (a + b + c);
+        areas(i) = sqrt(s * (s - a) * (s - b) * (s - c)); % Heron's formula
+    end
+
+    % Computing the cumulative distribution function (CDF) for areas
+    total_area = sum(areas);
+    probs = areas / total_area;
+    cdf = cumsum(probs);
+
+    % Sampling points based on triangle areas
+    sampled_points = zeros(num_samples, 3);
+    for i = 1:num_samples
+        r = rand(); % Random value for selecting triangle based on area
+        triangle_index = find(cdf >= r, 1, 'first'); % Find the triangle to sample from
+
+        % Get vertices for the selected triangle
+        v1 = vertices(:, triangulated_faces(1, triangle_index));
+        v2 = vertices(:, triangulated_faces(2, triangle_index));
+        v3 = vertices(:, triangulated_faces(3, triangle_index));
+
+        % Barycentric coordinates for random point inside triangle
+        sqrt_r1 = sqrt(rand());
+        r2 = rand();
+        point = (1 - sqrt_r1) * v1 + (sqrt_r1 * (1 - r2)) * v2 + (sqrt_r1 * r2) * v3;
+
+        sampled_points(i, :) = point;
+    end
+end
 
         function normalized_node_xyz = normalize_object(obj, translated_node_xyz)
             n = size(translated_node_xyz, 2);
